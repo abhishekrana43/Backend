e3a5ebcae42a04698d424ae4cf7dd62fd61476a4
Abhishek Rana <abhishekrana0743@gmail.com>
2025-08-19 21:21:15 +0530

    fix all register, controller bugs

--- src/db/index.js
+++ src/db/index.js
@@ -5,9 +5,9 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async() => {
     try {
-     const connectionInstanec = await mongoose.connect(`${process.env.MONGODB_URI}/ ${DB_NAME}`)
+     const connectionInstanec = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
 
-     console.log(`\ MongoDB coonected !! DB HOST: ${connectionInstanec.connection.host}`);
+     console.log(`MongoDB coonected! DB HOST: ${connectionInstanec.connection.host}`);
     } catch (error) {
         console.log("MONGODB connection Failed", error);
         process.exit()
